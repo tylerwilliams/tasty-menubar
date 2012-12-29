8926690316a728fac8fbb935d4321baf2de446a6
Tyler Williams <tylerw@mishipeshu.(none)>
2012-12-29 17:49:56 -0500

    Add these (which used to be referenced in the old project)

---
+++ tasty/PreferencesController.h
@@ -0,0 +1,20 @@
+//
+//  PreferencesController.h
+//  tasty-menubar-cocoa
+//
+//  Created by Tyler Williams on 12/19/12.
+//  Copyright (c) 2012 Tyler Williams. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "NICInfoSummary.h"
+
+@interface PreferencesController : NSWindowController {
+    NSUserDefaults *preferences;
+}
+
+- (double) pollInterval;
+- (id) getUserDefault:(NSString *)prefKey;
+- (NSString *) macAddressString;
+
+@end
---
+++ tasty/PreferencesController.m
@@ -0,0 +1,50 @@
+//
+//  PreferencesController.m
+//  tasty-menubar-cocoa
+//
+//  Created by Tyler Williams on 12/19/12.
+//  Copyright (c) 2012 Tyler Williams. All rights reserved.
+//
+
+#import "PreferencesController.h"
+
+@implementation PreferencesController
+
+- (void) setDefaults {
+    // load the default prefs from "DefaultPrefs.plist"
+    preferences = [NSUserDefaults standardUserDefaults];
+    NSString *file = [[NSBundle mainBundle]
+                      pathForResource:@"DefaultPrefs" ofType:@"plist"];
+    
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:file];
+    [preferences registerDefaults:dict];
+    [preferences synchronize];
+}
+
+- (id) init {
+    if(self = [super initWithWindowNibName:@"Preferences"]) {
+        [self setDefaults];
+    }
+    return self;
+}
+
+- (double) pollInterval {
+    NSNumber *pollingInterval = [[NSUserDefaults standardUserDefaults] objectForKey:@"pollingIntervalSeconds"];
+    return [pollingInterval doubleValue];
+}
+
+- (id) getUserDefault:(NSString *)prefKey {
+    return [[NSUserDefaults standardUserDefaults] objectForKey:prefKey];
+}
+
+- (NSString *) macAddressString {
+    NICInfoSummary *summary = [[NICInfoSummary alloc] init];
+    NICInfo* wifi_info = [summary findNICInfo:@"en0"];
+    return [wifi_info getMacAddressWithSeparator:@"-"];
+}
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+}
+
+@end
